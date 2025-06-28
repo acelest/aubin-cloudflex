2be88228135df5752d48d06caed482ed135823b4
acelest <2401600193@kluniversity.in>
2025-06-28 16:38:51 +0530

    vercel analytics @acelest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 import { GeistSans } from "geist/font";
 import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
